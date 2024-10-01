141ebf9b6bea10469dd97c142d5a94420e721d38
LHNT2k7 <ltri9573@gmail.com>
2024-10-01 10:16:32 -0700

    gfd

--- Classwork.java
+++ Classwork.java
@@ -1,7 +1,20 @@
+Import java.util.Scanner;
 public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    System.out.println("Hello");
+    Scanner sc = new Scanner (System.in);
+    System.out.println("What did you want to buy?");
+    String item;
+    Item = sc.nextline();
+    System.out.println("How many did you want to buy?");
+    int quantity;
+    quantity = sc.nextline();
+    System.out.println("How much it cost for 1 unit?");
+    double price;
+    price = sc.nextline();
+    System.out.println(quantity + item + "at" + );
+    
   }
 }
